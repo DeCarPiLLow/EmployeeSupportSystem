fbd03e5131f07290da50de66fe12e9b5669cda46
rednirma <amrindersinghsandhu2002@gmail.com>
2024-08-29 09:30:47 +0530

    updated analytics pages

--- EmployeeSupportSystem/Migrations/20240829032006_myinitialcreate.Designer.cs
+++ EmployeeSupportSystem/Migrations/20240829032006_myinitialcreate.Designer.cs
@@ -4,6 +4,7 @@ using EmployeeSupportSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeSupportSystem.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240829032006_myinitialcreate")]
+    partial class myinitialcreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
